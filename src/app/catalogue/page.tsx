db555cfaa1b91874c888e4c3e1994b2f5b28c278
PohongKejuGratisRoti <arrywahyu00@gmail.com>
2025-10-17 06:56:45 +0700

    jembut

--- src/app/catalogue/page.tsx
+++ src/app/catalogue/page.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import NavCat from '@/components/NavCat'
+import SearchBar from '@/components/SearchBar'
 
 const page = () => {
   return (
     <div className='bg-white'>
       <NavCat />
-      <div className='w-full bg-white h-[50vh]'> {/* tempat lebar untuk header */}
-
+      <div className='w-full bg-white h-[10vh] flex flex-row-reverse items-center'> {/* tempat lebar untuk header */}
+        <SearchBar />
+        <div></div>
+        <div></div>
+        {/*wrapper bodi dibawah navbar*/}
+        
       </div>
 
     </div>
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from 'react'
+
+const SearchBar = () => {
+  return (
+    <div className='w-32 border border-black text-black h-8'> tes</div>
+  )
+}
+
+export default SearchBar
