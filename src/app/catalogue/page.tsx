a8d5ab1293242593033bd2b0070ad1751abc3c00
PohongKejuGratisRoti <arrywahyu00@gmail.com>
2025-10-23 18:40:53 +0700

    Bismillah cuan

--- src/app/catalogue/page.tsx
+++ src/app/catalogue/page.tsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import NavCat from '@/components/NavCat'
 import SearchBar from '@/components/SearchBar'
+import ProductTray from '@/components/ProductTray'
 
 const page = () => {
   return (
     <div className='bg-white'>
       <NavCat />
-      <div className='w-full bg-white h-[10vh] flex flex-row-reverse items-center'> {/* tempat lebar untuk header */}
+      <div className='w-full bg-white h-[10vh] flex flex-row-reverse items-center text-black justify-evenly'> {/* tempat lebar untuk header */}
         <SearchBar />
-        <div></div>
-        <div></div>
-        {/*wrapper bodi dibawah navbar*/}
+        <ProductTray />
         
+        <div>Logo Perusahaan</div>
+        
+      </div>
+      {/*wrapper bodi dibawah navbar*/}
+      <div className='w-full h-screen bg-gray-50 border border-black flex flex-row text-black'>
+
+        <div>tes1</div>
+        <div>tes2</div>
+        
+
       </div>
 
     </div>
---
+++ src/components/ProductTray.tsx
@@ -0,0 +1,16 @@
+"use client";
+import React from 'react'
+import Link from 'next/link'
+
+const ProductTray = () => {
+  return (
+    <div className='flex flex-row-reverse text-black gap-3.5'>
+        <div>Produk 4</div>
+        <div>Produk 3</div>
+        <div>Produk 2</div>
+        <div>Produk 1</div>
+    </div>
+  )
+}
+
+export default ProductTray
